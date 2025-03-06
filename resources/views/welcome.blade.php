3605944914891ad70c50f4e6ad55466e0db5471a
princecalmishi <atdigitalafrica@gmail.com>
2025-03-06 12:44:30 +0300

    Updated welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,7 @@
 
 @section('content')
 
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8710537618632255"
-     crossorigin="anonymous"></script>
-<!-- header -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-8710537618632255"
-     data-ad-slot="4928047871"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<!-- google adsense here -->
 
 
 @include('inc.searchandfilter')
